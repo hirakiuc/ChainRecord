2ada2c12f3cedbe397da9a3a44c0161a44d58d42
hirakiuc <hirakiuc@gmail.com>
2010-04-04 11:44:48 +0900

    疎通時の修正

--- src/lib/meta_driver_factory.php
+++ src/lib/meta_driver_factory.php
@@ -1,15 +1,27 @@
 <?php
+require_once "errors.php";
+require_once "metadriver/pgsql_meta_driver.php";
+require_once "metadriver/mysql_meta_driver.php";
 
 class MetaDriverFactory{
-
+    /**  */
     private static $instance = null;
 
+    /** 
+     * array($table_name => $props,...)
+     */
     private $cache = null;
 
+    /**
+     *
+     */
     private function __construct(){
         $this->cache = array();
     }
 
+    /**
+     *
+     */
     public static function getInstance(){
         if(is_null(MetaDriverFactory::$instance)){
             MetaDriverFactory::$instance = new MetaDriverFactory();
@@ -17,15 +29,21 @@ class MetaDriverFactory{
         return MetaDriverFactory::$instance;
     }
 
-    public function getTableColumns($pdo, $model){
-        $table_name = $model->table_name;
+    /**
+     *
+     */
+    public function getColumns($pdo, $table_name){
+
+        if($this->isCached($table_name)){
+            return $this->cache[$table_name];
+        }
 
         $meta_driver = null;
-        $adapter = $model->pdo_config["adapter"];
+        $adapter = $pdo->getAttribute(PDO::ATTR_DRIVER_NAME);
         if($adapter === "mysql"){
-            
-        }else if($adapter === "postgresql"){
-
+            $meta_driver = new MySQLMetaDriver();
+        }else if($adapter === "pgsql"){
+            $meta_driver = new PgSQLMetaDriver();
         }
 
         if(is_null($meta_driver)){
@@ -33,7 +51,31 @@ class MetaDriverFactory{
             throw new NotSupportDbError($msg);
         }
 
+        $props = $meta_driver->getColumns($pdo, $table_name);
+
+        $this->addCache($table_name, $props);
+
+        return $props;
     }
+
+    /**
+     *
+     */
+    private function isCached($table_name){
+        return isset($this->cache[$table_name]);
+    }
+
+    /**
+     *
+     */
+    private function addCache($table_name, $props){
+        $this->cache[$table_name] = $props;
+    }
+}
+
+
+abstract class MetaDriver {
+    abstract public function getColumns($pdo, $table_name);
 }
 
 ?>
