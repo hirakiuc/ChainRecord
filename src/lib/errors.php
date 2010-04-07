85f1452b49ba05726e5395c88d226dcf0f0e751a
hirakiuc <hirakiuc@gmail.com>
2010-04-07 21:28:46 +0900

    rename className.

--- src/lib/errors.php
+++ src/lib/errors.php
@@ -14,6 +14,6 @@ class NotSupportError extends CRError{}
 
 class TableNotFoundError extends CRError{}
 
-class ColumnNotFoundError extends CRError{}
+class NoSuchPropertyError extends CRError{}
 
 ?>
