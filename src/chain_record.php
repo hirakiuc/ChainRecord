6d27690b156526d0a623c330926e5a8fff1b8d7c
hirakiuc <hirakiuc@gmail.com>
2010-04-07 21:29:30 +0900

    crudをざっくり実装(動作未確認)

--- src/chain_record.php
+++ src/chain_record.php
@@ -10,7 +10,7 @@ require_once "lib/inflector.php";
 class ChainRecord {
     
     /**  */
-    protected $primary_key = array(); 
+    protected $primary_key = array();
 
     /**  */
     protected $db_key = null;
@@ -87,6 +87,13 @@ class ChainRecord {
                 "is_updated" => false
             );
         } 
+
+        foreach($this->primary_key as $pkey){
+            if(!in_array($pkey, $this->column_names, true)){
+                $msg = "No Such property found: ".$pkey;
+                throw new NoSuchPropertyError($msg);
+            }
+        }
     }
 
 
@@ -95,12 +102,12 @@ class ChainRecord {
      *
      */
     public function create($ary){ 
-        foreach($ary as $key => $val){
-            $this->$key = $val;
-        }
-
-        foreach($this->vals as $name => $v){
-            $v["is_updated"] = false;
+        foreach($ary as $key => $v){ 
+            if(!$this->is_column_exist($key)){
+                $msg = $key." column is not defined.";
+                throw new NoSuchPropertyError($msg);
+            } 
+            $this->vals[$key]["value"] = $v; 
         } 
         return $this;
     }
@@ -142,11 +149,82 @@ class ChainRecord {
     }
 
     /**
-     *
+     * $ary(
+     *  "cond" => array("condition", array(params1,..)),
+     *  "order" => order_by_string
+     *  "limit" => number or array(limit, offset),
+     * )
      */
     public function find($ary){
-        // SQL:select
-        return new ChainTube(array());
+        $params = array();
+
+        $query = "SELECT * FROM ".$this->table_name." ";
+
+        $cond = "";
+        if(isset($ary["cond"])){
+            $v = $ary["cond"];
+
+            $cond.= "WHERE ".$v[0];
+            if(count($v) > 1){
+                array_shift($v);
+                $params = $v;
+            }
+        }
+        
+        if($this->is_pkey_set()){
+            if(strlen($cond) > 0){
+                $cond.= " AND ";
+            }else{
+                $cond = "WHERE ";
+            }
+            $v = $this->get_pkey_cond();
+
+            $cond.= $v["cond"];
+            $params = array_merge($params, $v["params"]);
+        }
+
+        if(strlen($cond) > 0){
+            $query.= $cond;
+        }
+
+        if(isset($ary["order"])){
+            $query.= " ORDER BY ".$ary["order"]." ";
+        }
+        
+        if(isset($ary["limit"])){
+            if(is_number($ary["limit"])){
+                $query.= " LIMIT ".$ary["limit"]." ";
+            }else{
+                $query.= " LIMIT " .$ary["limit"][0];
+                $query.= " OFFSET ".$ary["limit"][1];
+            }
+        }
+
+        try{
+            $stmt = $this->pdo->prepare($query);
+            if(is_null($stmt)){
+                throw new DBError($this->get_errmsg());
+            }
+
+            if(!$stmt->execute($params)){
+                throw new DBError($this->get_errmsg());
+            }
+
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $clazz = new ReflectionClass(get_class($this));
+
+            $objs = array();
+            foreach($rows as $row){
+                $v = $clazz->newInstance();
+                array_push($objs, $v->create($row));
+            }
+
+            return new ChainTube($objs); 
+
+        }catch(Exception $e){
+            throw $e;
+        } 
     }
 
     /**
@@ -159,12 +237,78 @@ class ChainRecord {
      * )
      */
     public function update($ary){
-        // SQL:update
         $params = array();
 
+        // accord arg value priority over $this property.
+        if(isset($ary["vals"])){
+            foreach($ary["vals"] as $name => $v){ 
+                if(!$this->is_column_exist($name)){
+                    $msg = $name." column is not defined.";
+                    throw new NoSuchPropertyError($msg);
+                } 
+                $this->vals[$name]["value"] = $v;
+                $this->vals[$name]["is_updated"] = true; 
+            }
+        }
 
+        // gather updated column value property.
+        $updated_props = array(); 
+        foreach($this->vals as $key => $v){
+            if($v["is_updated"]){
+                $updated_columns[$key] = $v["value"];
+            }
+        }
 
-        return 0;
+        $query = "UPDATE ".$this->table_name." SET ";
+
+        $columns = array();
+        foreach($updated_props as $name => $v){
+            array_push($columns, $name. " = ? ");
+            array_push($params, $v);
+        }
+        $query.= implode($columns, ",");
+
+        $cond = "";
+        if(isset($ary["cond"])){
+            $v = $ary["cond"];
+
+            $cond.= "WHERE ".$v[0];
+            if(count($v) > 1){
+                array_shift($v);
+                $params = $v;
+            }
+        }
+
+        if($this->is_pkey_set()){
+            if(strlen($cond) > 0){
+                $cond.= " AND ";
+            }else{
+                $cond = "WHERE ";
+            }
+            $v = $this->get_pkey_cond();
+
+            $cond.= $v["cond"];
+            $params = array_merge($params, $v["params"]);
+        }
+
+        if(strlen($cond) > 0){
+            $query.= $cond;
+        }
+
+        try{
+            $stmt = $this->pdo->prepare($query);
+            if(is_null($stmt)){
+                throw new DBError($this->get_errmsg());
+            }
+
+            if(!$stmt->execute($params)){
+                throw new DBError($this->get_errmsg());
+            }
+
+            return $stmt->rowCount();
+        }catch(Exception $e){
+            throw $e;
+        } 
     }
 
     /**
@@ -176,14 +320,34 @@ class ChainRecord {
         $params = array();
 
         $query = "DELETE FROM ". $this->table_name." ";
+
+        $cond = "";
         if(isset($ary["cond"])){
             $v = $ary["cond"];
 
-            $query.= "WHERE ".$v[0];
+            $cond.= "WHERE ".$v[0];
             if(count($v) > 1){
                 array_shift($v);
                 $params = $v;
             }
+        }
+
+        // add self pkey condition if $this model has primary key values.
+        // ("has" means "not null value")
+        if($this->is_pkey_set()){
+            if(strlen($cond) > 0){
+                $cond.= " AND ";
+            }else{
+                $cond = "WHERE ";
+            } 
+            $v = $this->get_pkey_cond();
+
+            $cond.= $v["cond"];
+            $params = array_merge($params, $v["params"]);
+        }
+
+        if(strlen($cond) > 0){
+            $query.= $cond;
         }
 
         try{
@@ -206,7 +370,7 @@ class ChainRecord {
     function __get($name){
         if(!$this->is_column_exist($name)){
             $msg = $name." column is not defined.";
-            throw new ColumnNotFoundError($msg);
+            throw new NoSuchPropertyError($msg);
         }    
         $v = $this->vals[$name]; 
         return $v["value"];
@@ -215,7 +379,7 @@ class ChainRecord {
     function __set($name, $value){
         if(!$this->is_column_exist($name)){
             $msg = $name." column is not defined.";
-            throw new ColumnNotFoundError($msg);
+            throw new NoSuchPropertyError($msg);
         } 
         $v = $this->vals[$name];
         $v["value"] = $value;
@@ -225,13 +389,6 @@ class ChainRecord {
     function __call($name, $parameters){
 
     }
-
-//    private function check_column_exist($name){
-//        if($this->is_column_exist($name)){
-//            $msg = "No Such Property exist: " .$name;
-//            throw new NoSuchPropertyError($msg);
-//        }
-//    }
 
     protected function get_errmsg(){
         $err = $this->pdo->errorInfo();
@@ -245,6 +402,38 @@ class ChainRecord {
     protected function is_column_exist($name){
         return array_key_exists($name, $this->props);
     }
+
+    protected function is_pkey_set(){
+        if(empty($this->primary_key)){
+            return false;
+        }
+
+        foreach($this->primary_key as $key_name){
+            $v = $this->vals[$key_name]["value"];
+            if(is_null($v)){
+                return fasle;
+            }
+        }
+
+        return true;
+    }
+
+    protected function get_pkey_cond(){
+        $params = array(); 
+
+        $v = array();
+        foreach($this->primary_key as $pkey){
+            array_push($v, $pkey." = ? ");
+            array_push($params, $this->vals[$pkey]["value"]);
+        }
+
+        $cond = " (". implode($v, " AND ") . ") "; 
+
+        return array(
+            "cond"   => $cond,
+            "params" => $params
+        );
+    } 
 }
 
 /* vim: set expandtab sw=4 st=4 ts=4 : */
