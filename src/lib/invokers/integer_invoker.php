998e31131479b8e9be262f8cb9563416e46e89fe
hirakiuc <hirakiuc@gmail.com>
2010-04-12 21:50:22 +0900

    ざっと実装(未疎通)

--- src/lib/invokers/integer_invoker.php
+++ src/lib/invokers/integer_invoker.php
@@ -2,55 +2,59 @@
 
 class IntegerInvoker extends MethodInvoker {
 
-  publc function max(){
-    $max = null;
-    foreach($this->list as $v){
-      if(is_null($max)){
-        $max = $v;
-      }
+    publc function max(){
+        $max = null;
+        foreach($this->list as $v){
+            if(is_null($max)){
+                $max = $v;
+            }
 
-      if($v > $max){
-        $max = $v;
-      }
+            if($v > $max){
+                $max = $v;
+            }
+        }
+
+        return $max;
     }
 
-    return $max;
-  }
+    public function min(){
+        $min = null;
+        foreach($this->list as $v){
+            if(is_null($min)){
+                $min = $v;
+            }
 
-  public function min(){
-    $min = null;
-    foreach($this->list as $v){
-      if(is_null($min)){
-        $min = $v;
-      }
-
-      if($v < $min){
-        $min = $v;
-      }
-    }
-    return $min;
-  }
-
-  public function avg(){
-    if(empty($this->list)){
-      return false;
+            if($v < $min){
+                $min = $v;
+            }
+        }
+        return $min;
     }
 
-    $sum = 0;
-    foreach($this->list as $v){
-      $sum += $v;
+    public function avg(){
+        if(empty($this->list)){
+            return false;
+        }
+
+        $sum = 0;
+        foreach($this->list as $v){
+            $sum += $v;
+        }
+
+        return $sum/count($this->list);
     }
 
-    return $sum/count($this->list);
-  }
-
-  public function to_s(){
-    $ary = array();
-    foreach($this->list as $v){
-      array_push($ary, strval($v));
+    public function sort($order){
+        // TODO implement
     }
-    return $this->list;
-  }
+
+    public function to_s(){
+        $ary = array();
+        foreach($this->list as $v){
+            array_push($ary, strval($v));
+        }
+        return $this->list;
+    }
 }
 
 
