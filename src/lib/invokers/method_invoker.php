a7950446e36a59a231bc15dd6b1902e3a89142d7
hirakiuc <hirakiuc@gmail.com>
2010-04-12 21:49:33 +0900

    commented todo...

--- src/lib/invokers/method_invoker.php
+++ src/lib/invokers/method_invoker.php
@@ -4,32 +4,37 @@ require_once "errors.php";
 
 class MethodInvoker{
 
-  protected $list = null;
+    protected $list = null;
 
-  function __construct($ary){
-    $this->list = $ary;
-  }
-
-  public function invoke($name, $paramters){
-    $myself = new ReflectionClass(get_class($this));
-    if($myself->hasMethod($name)){ 
-      $results = array();
-
-      foreach($this->list as $v){
-        $ret = call_user_func_array(
-                array($this,$name), $parameters);
-        array_push($results, $ret);
-      } 
-
-      return $results;
-    }else{
-      $msg = "Undefined Method Called:";
-      $msg.= "(class:".get_class($this).", method:".$name.")";
-      throw new Exception($msg);
+    function __construct($ary){
+        $this->list = $ary;
     }
-  }
 
-}
+    public function invoke($name, $paramters){
+        $myself = new ReflectionClass(get_class($this));
+        if($myself->hasMethod($name)){ 
+            $results = array();
 
+            // TODO grouping by target database.
+            // TODO call query optimizer if object is subclass of ChainRecord.
+            // TODO make $this->list contains one object and pass optimized cond arg. 
+
+            foreach($this->list as $v){
+                $ret = call_user_func_array(
+                    array($this,$name), $parameters);
+                array_push($results, $ret);
+            } 
+
+            // TODO check whether called method is "find" and args has "order" opt.
+            // TODO call sorter if $ret has object which is subclass of ChainRecord.
+
+            return $results;
+        }else{
+            $msg = "Undefined Method Called:";
+            $msg.= "(class:".get_class($this).", method:".$name.")";
+            throw new Exception($msg);
+        }
+    } 
+} 
 
 ?>
