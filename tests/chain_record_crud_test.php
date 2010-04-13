f14536de962e85e37f11fc578aed088147f74a50
hirakiuc <hirakiuc@gmail.com>
2010-04-13 21:50:26 +0900

    add test case

--- tests/chain_record_crud_test.php
+++ tests/chain_record_crud_test.php
@@ -27,6 +27,22 @@ class ChainRecordCRUDTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals(count($grps), 10);
     }
 
+    public function testFind_all_property(){
+        $obj = new Group();
+
+        $grps = $obj->find();
+        $v = $grps[0];
+
+        $i = 0;
+        foreach($grps as $m){ 
+            $this->assertNotNull($m->id);
+            $this->assertEquals("title".$i, $m->title);
+            $this->assertEquals("memomemo", $m->memo);
+
+            $i+=1;
+        }
+    }
+
     public function testFind_limit(){
         $obj = new Group();
 
@@ -99,32 +115,75 @@ class ChainRecordCRUDTest extends PHPUnit_Framework_TestCase {
         } 
     }
 
-    public function testFind_all_property(){
+    public function testFind_cond(){
         $obj = new Group();
 
-        $grps = $obj->find();
-        $v = $grps[0];
+        $grps = $obj->find(array(
+                "cond" => array("title = 'title2'")
+            )
+        );
 
-        $i = 0;
-        foreach($grps as $m){ 
-            $this->assertNotNull($m->id);
-            $this->assertEquals("title".$i, $m->title);
-            $this->assertEquals("memomemo", $m->memo);
-
-            $i+=1;
-        }
+        $this->assertEquals(1, count($grps));
+        $this->assertEquals("title2", $grps[0]->title);
     }
+
+    public function testFind_cond_with_param(){
+        $obj = new Group();
+        $p1 = "title2";
+
+        $grps = $obj->find(array(
+                "cond" => array("title = ?", $p1)
+            )
+        );
+
+        $this->assertEquals(1, count($grps));
+        $this->assertEquals("title2", $grps[0]->title);
+    }
+
+    public function testFind_cond_with_params(){
+        $obj = new Group();
+        $p1 = "title2";
+        $p2 = "memomemo";
+
+        $grps = $obj->find(array(
+                "cond" => array("title = ? AND memo = ?", $p1, $p2)
+            )
+        );
+
+        $this->assertEquals(1, count($grps));
+        $this->assertEquals("title2", $grps[0]->title);
+        $this->assertEquals("memomemo", $grps[0]->memo);
+    }
+
+    public function testFind_with_pkey(){
+        $obj = new Group();
+
+        $grps = $obj->find(array(
+                "limit" => 1
+            )
+        );
+
+        $this->assertEquals(1, count($grps));
+        $grp = $grps[0];
+        $before_id = $grp->id;
+
+        $after_grps = $grp->find();
+        $this->assertEquals(1, count($after_grps));
+        $this->assertEquals($before_id, $after_grps[0]->id); 
+
+        $obj2 = new Group();
+        $obj2->id = $before_id;
+
+        $after2_grps = $obj2->find();
+        $this->assertEquals(1, count($after2_grps));
+        $this->assertEquals($before_id, $after2_grps[0]->id);
+    }
+
 
     protected function tearDown(){
-        $grp = new Group();
-
+        $grp = new Group(); 
         $grp->destroy();
-    }
-
-}
-
-
-
-
+    } 
+} 
 
 ?>
