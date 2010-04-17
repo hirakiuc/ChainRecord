752dc1047155a75b8e30465792371115e9ac8601
hirakiuc <hirakiuc@gmail.com>
2010-04-17 18:59:17 +0900

    delete comments.

--- src/lib/invokers/method_invoker.php
+++ src/lib/invokers/method_invoker.php
@@ -11,22 +11,16 @@ class MethodInvoker{
     }
 
     public function invoke($name, $paramters){
+
         $myself = new ReflectionClass(get_class($this));
         if($myself->hasMethod($name)){ 
             $results = array();
-
-            // TODO grouping by target database.
-            // TODO call query optimizer if object is subclass of ChainRecord.
-            // TODO make $this->list contains one object and pass optimized cond arg. 
 
             foreach($this->list as $v){
                 $ret = call_user_func_array(
                     array($this,$name), $parameters);
                 array_push($results, $ret);
             } 
-
-            // TODO check whether called method is "find" and args has "order" opt.
-            // TODO call sorter if $ret has object which is subclass of ChainRecord.
 
             return $results;
         }else{
